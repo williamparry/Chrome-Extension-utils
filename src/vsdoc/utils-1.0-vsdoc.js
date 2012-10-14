359c1085c26ae83db9847aebab32a700cb59d785
William Parry <williamparry@gmail.com>
2012-10-14 22:21:59 +1100

    Added the start of some vs-doc helpers
    Chrome Browser Action
    Start of Utils vsdoc

---
+++ src/vsdoc/utils-1.0-vsdoc.js
@@ -0,0 +1,25 @@
+﻿var UTILS = {
+
+    // ------------------------------------------------------------------
+    // Object Helpers
+    // ------------------------------------------------------------------
+
+    OBJ: {
+
+        updateByPath: function (obj, keyStr, value) {
+            /// <summary>Update a property of an object that could be inner</summary>
+            /// <param name="Object" type="Object">The object to update</param>
+            /// <param name="Key path" type="String">e.g. prop1.innerProp2.innerProp3</param>
+            /// <param name="value" type="*"></param>
+        },
+
+        getByPath: function (obj, key, remaining) {
+            /// <summary>Retrieve a property of an object that could be inner</summary>
+            /// <param name="Object" type="Object">The object to update</param>
+            /// <param name="Key path" type="String">e.g. prop1.innerProp2.innerProp3</param>
+            /// <param name="value" type="*"></param>
+        }
+
+    }
+
+}
